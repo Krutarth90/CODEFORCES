806f3d4859f0e085eba020af50424426c693f7ce
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2024-08-09 20:05:32 +0000

    1703F - Yet Another Problem About Pairs Satisfying an Inequality

---
+++ SOLVED/1300/1703F - Yet Another Problem About Pairs Satisfying an Inequality.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define flp(k,n)                            for(int i=k;i<n;i++)
+#define flm(k,n)                            for(int j=n-1;j>=k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define int                                 long long
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+
+
+vector<int> prime;
+class solution
+{
+    public:
+    static void seive (int l)
+    {
+        vector<bool> check(l + 1, true);
+        check[0]=0;
+        check[1]=0;
+        for(int i = 2 ; i * i <=l ; i++)
+        {
+            if(check[i])
+            {
+                prime.pb(i);
+                for(int j = i * i ; j<=l ; j+=i)
+                    check[j]=0;
+            }
+        }
+        for(int i=sqrt(l) + 1 ; i <=l ; i++)
+            if(check[i])
+                prime.pb(i);
+    }
+
+    static int MOD(int n)
+    {
+        if(n>=0)
+            return n%((int)1e9 + 7) ;
+        else
+            return ((int)1e9 + 7) + n%((int)1e9 + 7) ;
+    } 
+
+    static void solve()
+    {
+        int n, ans = 0;
+        cin >> n;
+        int a[n];
+        vector<int> v;;
+        flp(0, n)
+        {
+            cin >> a[i];
+            if(a[i]>=i+1)
+                continue;
+            ans += lower_bound(v.begin(), v.end(), a[i]) - v.begin();
+            v.pb(i+1);
+        }
+        cout << ans << endl;
+    }
+};
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC=1 ;
+    cin>>TC ;
+    while(TC--)
+        solution::solve() ;
+}
