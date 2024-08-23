f23f6d32185d94c47975dcd8b1a359f00fa9e597
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2024-08-23 13:35:34 +0000

    1794C - Scoring Subsequences

---
+++ SOLVED/1300/1794C - Scoring Subsequences.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define flp(k,n)                            for(int i=k;i<n;i++)
+#define flm(k,n)                            for(int i=n-1;i>=k;i--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define int                                 long long
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+
+
+vector<int> prime;
+class solution
+{
+    public:
+    static void seive (int l)
+    {
+        vector<bool> check(l + 1, true);
+        check[0]=0;
+        check[1]=0;
+        for(int i = 2 ; i * i <=l ; i++)
+        {
+            if(check[i])
+            {
+                prime.pb(i);
+                for(int j = i * i ; j<=l ; j+=i)
+                    check[j]=0;
+            }
+        }
+        for(int i=sqrt(l) + 1 ; i <=l ; i++)
+            if(check[i])
+                prime.pb(i);
+    }
+
+    static int MOD(int n)
+    {
+        if(n>=0)
+            return n%((int)1e9 + 7) ;
+        else
+            return ((int)1e9 + 7) + n%((int)1e9 + 7) ;
+    }
+    static bool comp(pair<int,int>a,pair<int,int>b) 
+    {
+        if(a.first==b.first)
+            return a.second>b.second;
+        return a.first<b.first;
+    }
+    static int fact(int n)
+    {
+        int ans=1;
+        while(n)
+        {
+            ans*=n;
+            n--;
+        }
+        return ans;
+    }
+    static void solve()
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        flp(0,n)
+            cin>>a[i];
+        int ri=n;
+        int ans[n];
+        ans[0]=1;
+        flm(1,n)
+        {
+            int l = -1, r = ri;
+            while(l + 1 < r)
+            {
+                int m = (r + l) / 2;
+                if(a[m] / (i - m + 1) >= 1)
+                    r = m;
+                else
+                    l = m;
+            }
+            ans[i] = i - r + 1;
+            ri = r ;
+        }
+        flp(0,n)
+            cout<<ans[i]<<" ";
+        NL;
+    }
+};
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC=1 ;
+    cin>>TC ;
+    while(TC--)
+        solution::solve() ;
+}
