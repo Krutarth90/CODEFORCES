c749433f6eefd40dfc74b7fa182eae399bf9901c
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2024-10-11 21:20:57 +0530

    Create 414B - Mashmokh and ACM.cpp

---
+++ SOLVED/1400/414B - Mashmokh and ACM.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define flp(k,n)                            for(int i=k;i<n;i++)
+#define flm(k,n)                            for(int j=n-1;j>=k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define int                                 long long
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+
+
+vector<vector<int>> dp(2001, vector<int> (2001, -1));
+vector<int> prime;
+class solution
+{
+    public:
+    static void seive (int l)
+    {
+        vector<bool> check(l + 1, true);
+        check[0] = 0;
+        check[1] = 0;
+        for(int i = 2 ; i * i <= l ; i ++)
+        {
+            if(check[i])
+            {
+                prime.pb(i);
+                for(int j = i * i ; j <= l ; j += i)
+                    check[j] = 0;
+            }
+        }
+        for(int i = sqrt(l) + 1 ; i <= l ; i ++)
+            if(check[i])
+                prime.pb(i);
+    }
+
+    static int MOD(int n)
+    {
+        int mod = 1e9 + 7;
+        if(n >= 0)
+            return n % mod ;
+        else
+            return mod + n % mod ;
+    }
+    
+    static bool comp(pair<int, int>a, pair<int, int>b) 
+    {
+        if(a.first == b.first)
+            return a.second > b.second;
+        return a.first < b.first;
+    }
+    
+    static int fact(int n)
+    {
+        int ans = 1;
+        while(n > 1)
+        {
+            ans *= n;
+            n --;
+        }
+        return ans;
+    }
+
+    static int dps(int n, int l, int i)
+    {
+        if(l <= 0)
+            return 1;
+        if(dp[i][l] != -1)
+            return dp[i][l];
+        int ans = 0;
+        for(int j = i; j <= n; j += i)
+        {
+            ans = MOD(ans + dps(n, l - 1, j));
+        }
+        return dp[i][l] = ans;
+    }
+    
+    static void solve()
+    {
+        int n, k, ans = 0;
+        cin >> n >> k;
+        cout << dps(n, k, 1) << endl;
+    }
+};
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    //cin >> TC ;
+    while(TC--)
+        solution::solve() ;
+}
