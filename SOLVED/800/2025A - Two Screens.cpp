b4d67762a5d2bed26c4d18dd6de0e5f6a938c54b
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2024-10-14 22:38:29 +0530

    Create 2025A - Two Screens.cpp

---
+++ SOLVED/800/2025A - Two Screens.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define flp(k,n)                            for(int i=k;i<n;i++)
+#define flm(k,n)                            for(int j=n-1;j>=k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define int                                 long long
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+
+
+
+vector<int> prime;
+class solution
+{
+    public:
+    static void seive (int l)
+    {
+        vector<bool> check(l + 1, true);
+        check[0] = 0;
+        check[1] = 0;
+        for(int i = 2 ; i * i <= l ; i ++)
+        {
+            if(check[i])
+            {
+                prime.pb(i);
+                for(int j = i * i ; j <= l ; j += i)
+                    check[j] = 0;
+            }
+        }
+        for(int i = sqrt(l) + 1 ; i <= l ; i ++)
+            if(check[i])
+                prime.pb(i);
+    }
+
+    static int MOD(int n)
+    {
+        int mod = 1e9 + 7;
+        if(n >= 0)
+            return n % mod ;
+        else
+            return mod + n % mod ;
+    }
+    
+    static bool comp(pair<int, int>a, pair<int, int>b) 
+    {
+        if(a.first == b.first)
+            return a.second > b.second;
+        return a.first < b.first;
+    }
+    
+    static int fact(int n)
+    {
+        int ans = 1;
+        while(n > 1)
+        {
+            ans *= n;
+            n --;
+        }
+        return ans;
+    }
+    
+    static void solve()
+    {
+        string s, t;
+        cin >> s >> t;
+        int ans = 0, c = 0;
+        flp(0, min(s.length(), t.length()))
+        {
+            if(s[i] != t[i])
+                break;
+            c++;
+        }
+        cout << c + 1 * (c > 0) + s.length() - c + t.length() - c<< endl;
+    }
+};
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solution::solve() ;
+}
