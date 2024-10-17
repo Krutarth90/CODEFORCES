dd55fca609813da693dc6983f54b16139b752c9e
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2024-10-18 02:00:58 +0530

    Create 1350B - Orac and Models.cpp

---
+++ SOLVED/1400/1350B - Orac and Models.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define flp(k,n)                            for(int i=k;i<n;i++)
+#define flm(k,n)                            for(int j=n-1;j>=k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define int                                 long long
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+
+
+vector<int> prime;
+class solution
+{
+    public:
+    static void seive (int l)
+    {
+        vector<bool> check(l + 1, true);
+        check[0] = 0;
+        check[1] = 0;
+        for(int i = 2 ; i * i <= l ; i ++)
+        {
+            if(check[i])
+            {
+                prime.pb(i);
+                for(int j = i * i ; j <= l ; j += i)
+                    check[j] = 0;
+            }
+        }
+        for(int i = sqrt(l) + 1 ; i <= l ; i ++)
+            if(check[i])
+                prime.pb(i);
+    }
+
+    static int MOD(int n)
+    {
+        int mod = 1e9 + 7;
+        if(n >= 0)
+            return n % mod ;
+        else
+            return mod + n % mod ;
+    }
+    
+    static bool comp(pair<int, int>a, pair<int, int>b) 
+    {
+        if(a.first == b.first)
+            return a.second > b.second;
+        return a.first < b.first;
+    }
+    
+    static int fact(int n)
+    {
+        int ans = 1;
+        while(n > 1)
+        {
+            ans *= n;
+            n --;
+        }
+        return ans;
+    }
+    
+    static void solve()
+    {
+        int n, ma = 1;
+        cin >> n;
+        vector<int> dp(n + 1, -1), arr(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+        flp(1, n + 1)
+        {
+            cin >> arr[i];
+            dps(dp, arr, i, ma);
+        }
+        cout << ma << endl;
+    }
+
+    static int dps(vector<int>& dp, vector<int>& a, int k, int & ma)
+    {    
+        if(dp[k] != -1)
+            return dp[k];
+        int ans = 1;
+        for(int i = 2; i * i <= k; i ++)
+        {
+            if(k % i)
+                continue;
+            if(a[k] > a[i])
+                ans = max(ans, 1 + dps(dp, a, i, ma));
+            if(a[k] > a[k / i])
+                ans = max(ans, 1 + dps(dp, a, k / i, ma));
+        }
+        if(a[k] > a[1])
+            ans = max(ans, 1 + dps(dp, a, 1, ma));
+        ma = max(ma, ans);
+        return dp[k] = ans;
+    }
+};
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solution::solve() ;
+}
