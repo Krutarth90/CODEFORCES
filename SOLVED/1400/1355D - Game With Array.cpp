0f85b984c2d468b486c639e8fb90fd8f2f12d9c5
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2024-11-08 19:23:06 +0530

    Create 1355D - Game With Array.cpp

---
+++ SOLVED/1400/1355D - Game With Array.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int                                 long long
+#define flp(k,n)                            for(int i=(int)k;i<(int)n;i++)
+#define flm(k,n)                            for(int j=(int)n-1;j>=(int)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+ 
+
+void solve()
+{
+    int n, s;
+    cin >> n >> s;  
+    if(s >= 2 * n)
+    {
+        YES;
+        flp(0, n - 1)
+            cout << 2 << " ";
+        cout << s - (2 * (n - 1)) << endl << 1;
+    }
+    else
+        NO;
+}
+
+
+ 
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    //cin >> TC ;
+    while(TC--)
+        solve() ;
+}
