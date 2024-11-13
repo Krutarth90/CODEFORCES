078ae4fe1f3ae6c7d3a0292aaf6dcdc279e591d2
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2024-11-13 23:11:21 +0530

    Create 265A - Colorful Stones (Simplified Edition).cpp

---
+++ SOLVED/800/265A - Colorful Stones (Simplified Edition).cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int                                 long long
+#define flp(k,n)                            for(int i=(int)k;i<(int)n;i++)
+#define flm(k,n)                            for(int j=(int)n-1;j>=(int)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+ 
+
+void solve()
+{
+    string s, t;
+    cin >> s >> t;
+    int c = 0;
+    flp(0, t.size())
+    {
+        if(s[c] == t[i])
+            c ++;
+    }
+    cout << c + 1 << endl;
+}
+
+
+ 
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    //cin >> TC ;
+    while(TC--)
+        solve() ;
+}
