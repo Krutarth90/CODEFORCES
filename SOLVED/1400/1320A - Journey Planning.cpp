9ed2f969a6f2fbf972ee1b80c53f90d669849087
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2024-12-11 21:33:44 +0530

    Create 1320A - Journey Planning.cpp

---
+++ SOLVED/1400/1320A - Journey Planning.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int                                 long long
+#define flp(k,n)                            for(int i=(int)k;i<(int)n;i++)
+#define flm(k,n)                            for(int j=(int)n-1;j>=(int)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+ 
+
+void solve()
+{
+    int n, m = 0;
+    cin >> n;
+    map<int, int> ma;
+    flp(1, n + 1)
+    {
+        int x;
+        cin >> x;
+        ma[i - x] += x;
+        m = max(m ,ma[i - x]);
+    }
+    cout << m << endl;
+}
+
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    //cin >> TC ;
+    while(TC--)
+        solve() ;
+}
