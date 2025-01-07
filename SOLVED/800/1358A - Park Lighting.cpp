d371cee3147d05f86cfd2919efa532d914743d0f
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-01-07 23:50:26 +0530

    Create 1358A - Park Lighting.cpp

---
+++ SOLVED/800/1358A - Park Lighting.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int                                 long long
+#define flp(k,n)                            for(int i=(int)k;i<(int)n;i++)
+#define flm(k,n)                            for(int j=(int)n-1;j>=(int)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+ 
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    cout << (n * m + 1) / 2 << endl;    
+}
+
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solve() ;
+}
