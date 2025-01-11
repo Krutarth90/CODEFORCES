a5c73cfe9061de877f1fa7713931fb82974ce2b7
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-01-11 10:23:59 +0530

    Create 1354C1 - Simple Polygon Embedding.cpp

---
+++ SOLVED/1400/1354C1 - Simple Polygon Embedding.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int                                 long long
+#define flp(k,n)                            for(int i=(int)k;i<(int)n;i++)
+#define flm(k,n)                            for(int j=(int)n-1;j>=(int)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+
+
+
+
+
+#define M_PI 3.14159265358979323846
+void solve()
+{
+    int n;
+    cin >> n;
+    double ans = 1 / tan(M_PI /(2 * n));
+    cout << setprecision(6) << fixed;
+    cout << ans << endl;
+}
+
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solve() ;
+}
