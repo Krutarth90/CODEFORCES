63bb64e9faf6b701383689fb45ca9f3be23949c5
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-02-07 22:13:18 +0530

    Create 166E - Tetrahedron.cpp

---
+++ SOLVED/1500/166E - Tetrahedron.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int                                 long long
+#define flp(k,n)                            for(int i=(int)k;i<(int)n;i++)
+#define flm(k,n)                            for(int j=(int)n-1;j>=(int)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+
+void solve()
+{
+    int n, d = 1, abc = 0;
+	cin >> n;
+	flp(1, n + 1)
+	{
+		int nd = (3 * abc) % ((int)1e9 + 7), nabc = (d + (2 * abc)) % ((int) 1e9 + 7);
+		d = nd;
+		abc = nabc;	
+	}
+	cout << d << endl;	
+}
+
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    // cin >> TC ;
+    while(TC--)
+        solve() ;
+}
