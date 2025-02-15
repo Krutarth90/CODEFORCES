2e2241bf05a46d079716fc816954b047363ec289
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-02-15 16:28:49 +0530

    Create 1538F - Interesting Function.cpp

---
+++ SOLVED/1500/1538F - Interesting Function.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int                                 long long
+#define flp(k,n)                            for(int i=(int)k;i<(int)n;i++)
+#define flm(k,n)                            for(int j=(int)n-1;j>=(int)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+
+void solve()
+{
+	int l, r, ans, mul = 1;
+	cin >> l >> r;
+	ans = r - l;
+	l /= 10;
+	r /= 10;
+	while(r)
+	{
+		ans += r - l;
+		l /= 10;
+		r /= 10;
+	}
+	cout << ans << endl;
+}
+
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solve() ;
+}
