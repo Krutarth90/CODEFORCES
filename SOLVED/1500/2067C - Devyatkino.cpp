ef461e93f98475936769e1eb9526eab6a7a498f2
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-03-05 00:55:22 +0530

    Create 2067C - Devyatkino.cpp

---
+++ SOLVED/1500/2067C - Devyatkino.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    flp(0, 10)
+    {
+        string s = to_string(n - i);
+        int m = 0;
+        for(auto x : s)
+        {
+            if(x <= '7')   
+                m = max(m, x - '0');
+        }
+        if(i >= 7 - m)
+        {
+            cout << i << endl;
+            return;
+        }
+    }
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solve() ;
+}
