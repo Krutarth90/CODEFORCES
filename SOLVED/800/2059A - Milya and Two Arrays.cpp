e93bc4cbfd1b3d0555eb23a7f110de372da0765e
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-03-08 23:46:05 +0530

    Create 2059A - Milya and Two Arrays.cpp

---
+++ SOLVED/800/2059A - Milya and Two Arrays.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    set<ll> se, a, b;
+    flp(0, n)
+    {
+        ll x;
+        cin >> x;
+        a.insert(x);
+        se.insert(x);
+    }
+    flp(0, n)
+    {
+        ll x;
+        cin >> x;
+        b.insert(x);
+        se.insert(x);
+    }
+    if(a.size() * b.size() > 2)
+        YES;
+    else
+        NO;
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solve() ;
+}
