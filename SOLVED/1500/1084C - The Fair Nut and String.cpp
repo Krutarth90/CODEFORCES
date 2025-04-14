00315d5a7f0d0dd06783ee2d4dc0231554dd2154
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-04-14 20:50:46 +0530

    Create 1084C - The Fair Nut and String.cpp

---
+++ SOLVED/1500/1084C - The Fair Nut and String.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    bool f = 1;
+    ll ans = 0, c = 0;
+    string s;
+    cin >> s;
+    for(auto x : s)
+    {
+        if(x == 'b')
+        {
+            ans = (((c + 1) % ((ll)1e9 + 7)) * (ans % ((ll)1e9 + 7))) % ((ll)1e9 + 7);
+            ans = (ans + c) % ((ll)1e9 + 7);
+            c = 0;
+            f = 1;
+        }
+        else if(x == 'a')
+        {
+            c ++;
+        }
+    }
+    ans = (((c + 1) % ((ll)1e9 + 7)) * (ans % ((ll)1e9 + 7))) % ((ll)1e9 + 7);
+    ans = (ans + c) % ((ll)1e9 + 7);
+    cout << ans << endl;
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    // cin >> TC ;
+    while(TC--)
+        solve() ;
+}
