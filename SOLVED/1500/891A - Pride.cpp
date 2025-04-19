5de5e43c617ed651d2111706818d9a5362bad5c3
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-04-19 20:53:56 +0530

    Create 891A - Pride.cpp

---
+++ SOLVED/1500/891A - Pride.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    ll n, c = 0, mi = LLONG_MAX;
+    cin >> n;
+    vector<ll> a(n);
+    flp(0, n)
+    {
+        cin >> a[i];
+        if(a[i] == 1)
+            c ++;
+    }
+    if(c)
+    {
+        cout << n - c << endl;
+        return;
+    }
+    ll g = a[0];
+    flp(0, n)
+    {
+        ll t = a[i];
+        for(int j = i + 1; j < n; j ++)
+        {
+            if(j - i >= mi)
+                break;
+            t = __gcd(t, a[j]);
+            if(t == 1)
+            {
+                mi = min(mi, j - i + 1);
+                break;
+            }
+        }
+    }
+    if(mi == LLONG_MAX)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    cout << mi + n - 2 << endl;
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    // cin >> TC ;
+    while(TC--)
+        solve() ;
+}
