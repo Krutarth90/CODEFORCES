6d8ed6b5293aa66a1b2d67b9d3b468f6931ee944
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-04-20 21:22:23 +0530

    Create 2051A - Preparing for the Olympiad.cpp

---
+++ SOLVED/800/2051A - Preparing for the Olympiad.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (auto &x : a) 
+        cin >> x;
+    for (auto &x : b) 
+        cin >> x;
+    int ans = a[n - 1];
+    for (int i = 0; i < n - 1; ++i)
+      ans += max(0, a[i] - b[i + 1]);
+    cout << ans << '\n';
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    // cin >> TC ;
+    while(TC--)
+        solve() ;
+}
