bbfe59d414eabce3a48d1999bbe3670dd4d3d1f9
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-04-23 22:07:18 +0530

    Create 1398C - Good Subarrays.cpp

---
+++ SOLVED/1600/1398C - Good Subarrays.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    ll n, ans = 0;
+	cin >> n;
+	string s;
+	cin >> s;
+	ll sum = 0;
+	map<ll, ll> ma;
+	ma[1] ++;
+	flp(0, s.size())
+	{
+		sum += s[i] - '0';
+		ans += ma[sum - i];
+		ma[sum - i] ++;
+	}
+	cout << ans << endl;
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solve() ;
+}
