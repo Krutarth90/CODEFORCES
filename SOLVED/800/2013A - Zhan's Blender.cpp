1fd12c00fcd5a9b986e8366f3fe5e8a063b014df
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-05-02 23:53:03 +0530

    Create 2013A - Zhan's Blender.cpp

---
+++ SOLVED/800/2013A - Zhan's Blender.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    ll n, x, y;
+    cin >> n >> x >> y;
+    x = min(x, y);
+    cout << ((n / x) + ((n % x) > 0)) << endl;
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solve() ;
+}
