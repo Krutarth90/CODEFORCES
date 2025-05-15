feea2a87cc0cf462552e9e7d58e1aa59631b5f57
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-05-15 12:37:10 +0530

    Create 2104C - Card Game.cpp

---
+++ SOLVED/1100/2104C - Card Game.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll mod(ll x)
+{
+    return x % ((ll)1e9 + 7);
+}
+
+
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vector<string> ans= {"Alice", "Bob"};
+    if(s[0] == s[n - 1] || s[n - 1] == s[n - 2])
+    {
+        cout << ans[s[n - 1] - 'A'] << endl;
+        return;
+    }
+    ll a = 0, b = 0;
+    flp(0, n)
+        (s[i] == 'A' ? a : b) ++;
+    if(s.back() == 'B')
+    {
+        if(b == 1)
+        {
+            cout << ans[0] << endl;
+        }
+        else
+        {
+            cout << ans[1] << endl;
+        }
+    }
+    else
+    {
+        cout << ans[1] << endl;        
+    }
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solve() ;
+}
