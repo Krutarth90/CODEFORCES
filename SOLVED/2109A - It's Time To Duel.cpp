0324b5173f5e0f9ed751580feb2b6dfe478cc97e
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-05-23 22:49:02 +0530

    Create 2109A - It's Time To Duel.cpp

---
+++ SOLVED/2109A - It's Time To Duel.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll mod(ll x)
+{
+    return x % ((ll)1e9 + 7);
+}
+
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+
+    if (accumulate(a.begin(), a.end(), 0) == n) 
+    {
+        cout << "YES" << "\n";
+        return;
+    }
+
+    for (int i = 0; i < n - 1; i++) if (!a[i] && !a[i + 1]) 
+    {
+        cout << "YES" << "\n";
+        return;
+    }
+
+    cout << "NO" << "\n";
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solve() ;
+}
