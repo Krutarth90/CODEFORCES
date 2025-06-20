ae851d929632786a034ba5c70a1eb7aaf553c504
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-06-20 22:58:42 +0530

    Create 1809A - Garland.cpp

---
+++ SOLVED/800/1809A - Garland.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+
+
+void solve()
+{
+    bool f = 0;
+    string s;
+    cin >> s;
+    map<char, int> ma;
+    flp(0, 4)
+    {
+        ma[s[i]] ++;
+        if(ma[s[i]] == 4)
+        {
+            cout << -1 << endl;
+            return;
+        }
+        if(ma[s[i]] == 3)
+            f = 1;
+    }
+    if(f)
+        cout << 6 << endl;
+    else
+        cout << 4 << endl;
+        
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solve() ;
+}
