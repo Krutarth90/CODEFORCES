3c931e0941e57cfca60aa9cfdeded57a57eb4cd5
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-06-25 21:46:28 +0530

    Create 1005B - Delete from the Left.cpp

---
+++ SOLVED/900/1005B - Delete from the Left.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+
+
+void solve()
+{
+    string s, t;
+    cin >> s >> t;
+    int w = 0;
+    while (1) 
+    {
+        int i = s.length() - w - 1;
+        int j = t.length() - w - 1;
+        if (i >= 0 && j >= 0 && s[i] == t[j])
+            w++;
+        else
+            break;
+    }
+    cout << s.length() + t.length() - 2 * w << endl;
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    // cin >> TC ;
+    while(TC--)
+        solve() ;
+}
