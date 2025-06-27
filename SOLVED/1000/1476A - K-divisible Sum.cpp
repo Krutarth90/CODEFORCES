b3e83391a96ddc4e7275afc4b5c8db7cf28f99a4
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-06-28 00:16:15 +0530

    Create 1476A - K-divisible Sum.cpp

---
+++ SOLVED/1000/1476A - K-divisible Sum.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n)                            for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n)                            for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES                                 cout<<"YES\n"
+#define NO                                  cout<<"NO\n"
+#define endl                                "\n"
+#define pb                                  push_back
+#define ppb                                 pop_back
+#define NL                                  cout<<"\n"
+#define SPACE                               << " " << 
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds; 
+  
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+
+
+void solve()
+{
+    ll n, k, K,  ans;
+    cin >> n >> K;
+    k = K;
+    if(k == 1)
+    {
+        cout << 1 << endl;
+        return;
+    }
+    ll desiredSum = max(k, n);
+    if((desiredSum % k))
+        desiredSum += k - (desiredSum % k);
+    cout << desiredSum / n + ((desiredSum % n) > 0) << endl;
+}
+
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false) ; 
+    cin.tie(0) ;
+    int TC = 1 ;
+    cin >> TC ;
+    while(TC--)
+        solve() ;
+}
