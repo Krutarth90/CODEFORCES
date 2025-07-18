b5471d6295d74a14f2b6802ae1a59a0159774ef7
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-07-18 23:27:38 +0530

    Create 1008A - Romaji.cpp

---
+++ SOLVED/900/1008A - Romaji.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n) for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n) for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout<<"\n"
+#define SPACE << " " <<
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+
+void solve()
+{
+    string s;
+    cin >> s;
+    flp(0, s.size())
+    {
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'n')
+            continue;
+        if(i != s.size() - 1 && (s[i + 1] == 'a' || s[i + 1] == 'e' || s[i + 1] == 'i' || s[i + 1] == 'o' || s[i + 1] == 'u'))
+            continue;
+        NO;
+        return;
+    }
+    YES;
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    // cin >> TC;
+    while(TC--)
+        solve();
+}
