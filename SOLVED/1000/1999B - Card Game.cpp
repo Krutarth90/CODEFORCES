cbf340bc34c8096cf86dff7c43a6d0eb97313799
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-08-12 19:59:18 +0530

    Create 1999B - Card Game.cpp

---
+++ SOLVED/1000/1999B - Card Game.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n) for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n) for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout<<"\n"
+#define SPACE << " " <<
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+
+void solve()
+{
+    ll ans = 0;
+    vector<ll> a(2), b(2);
+    cin >> a[0] >> a[1] >> b[0] >> b[1];
+    flp(0, 2)
+    {
+        flm(0, 2)
+        {
+            if(a[i] > b[j])
+            {
+                if(i)
+                {
+                    if(j)
+                    {
+                        if(a[i - 1] >= b[j - 1])
+                            ans ++;
+                    }
+                    else if(a[i - 1] >= b[j + 1])
+                        ans ++;
+                }
+                else
+                {
+                    if(j)
+                    {
+                        if(a[i + 1] >= b[j - 1])
+                            ans ++;
+                    }
+                    else if(a[i + 1] >= b[j + 1])
+                        ans ++;
+                }
+            }    
+            else if(a[i] == b[j]) 
+            {
+                if(i)
+                {
+                    if(j)
+                    {
+                        if(a[i - 1] > b[j - 1])
+                            ans ++;
+                    }
+                    else if(a[i - 1] > b[j + 1])
+                        ans ++;
+                }
+                else
+                {
+                    if(j)
+                    {
+                        if(a[i + 1] > b[j - 1])
+                            ans ++;
+                    }
+                    else if(a[i + 1] > b[j + 1])
+                        ans ++;
+                }
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+}
