0f57c643f8716a402fab899f9b2273a1a668feb6
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-08-13 22:25:28 +0530

    Create 1931C - Make Equal Again.cpp

---
+++ SOLVED/1000/1931C - Make Equal Again.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n) for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n) for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout<<"\n"
+#define SPACE << " " <<
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+
+void solve()
+{
+    bool f = 1;
+    ll n, l = 0, r = 0;
+    cin >> n;
+    vector<ll> a(n);
+    flp(0, n)
+    {
+        cin >> a[i];
+        if(a[i] == a[0] && f)
+            l ++;
+        else
+            f = 0;
+    }
+    flm(0, n)
+    {
+        if(a[j] != a[n - 1])
+            break;
+        r ++;
+    }
+    if(a[0] != a[n - 1])
+    {
+        cout << n - max(l, r) << endl;
+        return;
+    }
+    cout << max(n - l - r, 0ll) << endl;
+
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+}
