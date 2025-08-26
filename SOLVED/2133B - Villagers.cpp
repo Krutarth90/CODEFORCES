cb016d8fad9884f16533aa87963d6289de2df668
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-08-26 23:05:14 +0530

    Create 2133B - Villagers.cpp

---
+++ SOLVED/2133B - Villagers.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n) for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n) for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout<<"\n"
+#define SPACE << " " <<
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+
+
+void solve()
+{
+    
+    ll n, ans = 0;
+    cin >> n;
+    vector<ll> a;
+    map<ll, ll> ma;
+    flp(0, n)
+    {
+        ll x;
+        cin >> x;
+        ma[x] ++;
+        if(ma[x] % 2 == 0)
+        {
+            ans += x;
+        }
+    }
+    for(auto[x, y] : ma)
+    {
+        if(y % 2 == 1)
+            a.pb(x);
+    }
+    for(ll i = 1 - a.size() % 2; i < a.size(); i += 2)
+        ans += a[i];
+
+    cout << ans << endl;
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+}
