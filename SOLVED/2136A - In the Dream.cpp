b5f7214d1838328e1d37393f1837a301c705c97d
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-09-01 23:54:16 +0530

    Create 2136A - In the Dream.cpp

---
+++ SOLVED/2136A - In the Dream.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n) for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n) for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout<<"\n"
+#define SPACE << " " <<
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+
+
+void solve()
+{
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    c -= a;
+    d -= b;
+    if(a < b)
+        swap(a, b);
+    if(a < 3 || a <= 2 * b + 2);
+    else
+    {
+        NO;
+        return;
+    }
+    if(c < d)
+        swap(c, d);
+    if(c < 3 || c <= 2 * d + 2)
+        YES;
+    else
+        NO;
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+}
