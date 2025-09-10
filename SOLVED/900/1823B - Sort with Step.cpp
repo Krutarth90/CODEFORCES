16bcc69fca76ae8a9ab1b132feeeec2e5b6d3573
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-09-10 23:24:30 +0530

    Create 1823B - Sort with Step.cpp

---
+++ SOLVED/900/1823B - Sort with Step.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n) for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n) for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout<<"\n"
+#define SPACE << " " <<
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+
+
+void solve()
+{
+    int n, k;
+	cin >> n >> k;
+	vector <int> p(n);
+	for (int i = 0; i < n; i++) 
+	{
+		cin >> p[i];
+		p[i]--;
+	}
+
+	int bad = 0;
+	for (int i=0; i<n; i++) 
+		if (p[i] % k != i % k) 
+			bad++;
+
+	if(bad == 0)
+		cout << 0 << '\n';
+	else if(bad == 2)
+		cout << 1 << '\n';
+	else
+		cout << -1 << '\n';
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+}
