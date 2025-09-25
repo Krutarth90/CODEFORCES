a9d094e8d2a4ebd3af69e2032b618daf60f2ac14
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-09-25 21:25:27 +0530

    Create 2050A - Line Breaks.cpp

---
+++ SOLVED/800/2050A - Line Breaks.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n) for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n) for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout<<"\n"
+#define SPACE << " " <<
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+void solve() 
+{
+    int n, m;
+    cin >> n >> m;
+    string s;
+    int ans =  0;
+    while(n--)
+    {
+        cin >> s;
+        ans += (m -= (int)s.length()) >= 0;
+    }
+    cout << ans << endl;
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int TC;
+    cin >> TC;
+    while (TC--) 
+        solve();
+    return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n) for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n) for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout<<"\n"
+#define SPACE << " " <<
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+void solve() 
+{
+    int n, m;
+    cin >> n >> m;
+    string s;
+    int ans =  0;
+    while(n--)
+    {
+        cin >> s;
+        ans += (m -= (int)s.length()) >= 0;
+    }
+    cout << ans << endl;
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int TC;
+    cin >> TC;
+    while (TC--) 
+        solve();
+    return 0;
+}
