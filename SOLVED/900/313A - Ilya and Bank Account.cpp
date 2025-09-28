21e3c17cc2efe9c1f98f73ebd0ed455d0700821a
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-09-28 20:42:48 +0530

    Create 313A - Ilya and Bank Account.cpp

---
+++ SOLVED/900/313A - Ilya and Bank Account.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k,n) for(ll i=(ll)k;i<(ll)n;i++)
+#define flm(k,n) for(ll j=(ll)n-1;j>=(ll)k;j--)
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout<<"\n"
+#define SPACE << " " <<
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+ 
+void solve() 
+{
+    ll n;
+    cin >> n;
+    if(n >= 0)
+    {
+        cout << n << endl;
+        return;
+    }
+    n *= -1;
+    ll dig1 = n % 10;
+    n /= 10;
+    ll dig2 = n % 10;
+    n /= 10;
+    n *= 10;
+    n += min(dig1, dig2);
+    cout << -n << endl;
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    // cin >> TC;
+    while(TC--)
+        solve();
+}
