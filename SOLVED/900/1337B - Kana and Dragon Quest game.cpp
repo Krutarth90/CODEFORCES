7e64021b89d2941813aad1e5b4fbf62c841d411b
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-10-29 19:46:55 +0530

    Create 1337B - Kana and Dragon Quest game.cpp

---
+++ SOLVED/900/1337B - Kana and Dragon Quest game.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k, n) for(ll i = (ll) k; i < (ll) n; i ++)
+#define flm(k, n) for(ll j = (ll) n - 1; j >= (ll) k; j --)
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout << "\n"
+#define SPACE << " " <<
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+ 
+void solve() 
+{
+    int x, n, m;
+	cin >> x >> n >> m;
+	while (x > 0 && n && x / 2 + 10 < x)
+    {
+        n --;
+        x = x / 2 + 10;
+    }
+	if (x <= m * 10)
+        YES;
+	else 
+        NO; 
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+}
