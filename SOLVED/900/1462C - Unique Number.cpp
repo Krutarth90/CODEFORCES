a61be50a63a5ae3ff9fcbc9f0cd19bf1be753438
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-11-03 23:29:03 +0530

    Create 1462C - Unique Number.cpp

---
+++ SOLVED/900/1462C - Unique Number.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k, n) for(ll i = (ll) k; i < (ll) n; i ++)
+#define flm(k, n) for(ll j = (ll) n - 1; j >= (ll) k; j --)
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout << "\n"
+#define SPACE << " " <<
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+
+
+void solve() 
+{
+    ll n;
+    cin >> n;
+    if(n > 45)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    string s = "";
+    for(int i = 9; i > 0; i --)
+    {
+        if(i > n)
+            continue;
+        s += ('0' + i);
+        n -= i;
+    }
+    reverse(s.begin(), s.end());
+    cout << s << endl;
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+}
