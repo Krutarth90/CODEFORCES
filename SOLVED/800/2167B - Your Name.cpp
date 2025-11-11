646918d03a64a8ac535fca06dcec9f48c7fae66c
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-11-11 19:54:27 +0530

    Create 2167B - Your Name.cpp

---
+++ SOLVED/800/2167B - Your Name.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k, n) for(ll i = (ll) k; i < (ll) n; i ++)
+#define flm(k, n) for(ll j = (ll) n - 1; j >= (ll) k; j --)
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout << "\n"
+#define SPACE << " " <<
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+
+
+void solve() 
+{
+    ll n;
+    cin >> n;
+    string s, t;
+    cin >> s >> t;
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    flp(0, n)
+    {
+        if(s[i] != t[i])
+        {
+            NO;
+            return;
+        }
+    }
+    YES;
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+}
