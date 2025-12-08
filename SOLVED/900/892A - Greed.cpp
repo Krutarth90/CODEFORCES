389bca235f2ac108f99723e06110d8bbfb242594
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-12-08 23:48:29 +0530

    Create 892A - Greed.cpp

---
+++ SOLVED/900/892A - Greed.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k, n) for(ll i = (ll) k; i < (ll) n; i ++)
+#define flm(k, n) for(ll j = (ll) n - 1; j >= (ll) k; j --)
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout << "\n"
+#define SPACE << " " <<
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+
+
+void solve() 
+{
+    ll n, s = 0, m1 = 0, m2 = 0;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    flp(0, n)
+    {
+        ll x;
+        cin >> x;
+        s += x;
+    }
+    flp(0, n)
+    {
+        ll x;
+        cin >> x;
+        if(x > m1)
+            swap(m1, x);
+        if(x > m2)
+            m2 = x;
+    }
+    if(s <= m1 + m2)
+        YES;
+    else
+        NO;
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    // cin >> TC;
+    while(TC--)
+        solve();
+}
