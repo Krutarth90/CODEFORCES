dbc86ef10b531d1fd80f44cb8fd0d6bee38769d7
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2025-12-18 22:06:09 +0530

    Create 1144B - Parity Alternated Deletions.cpp

---
+++ SOLVED/900/1144B - Parity Alternated Deletions.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k, n) for(ll i = (ll) k; i < (ll) n; i ++)
+#define flm(k, n) for(ll j = (ll) n - 1; j >= (ll) k; j --)
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout << "\n"
+#define SPACE << " " <<
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+
+
+void solve() 
+{
+	ll n;
+	cin >> n;
+    vector<ll> odd, even;
+	flp(0, n)
+	{
+		ll x;
+		cin >> x;
+		if(x % 2)
+			odd.pb(x);
+		else
+			even.pb(x);
+	}
+	if(odd.size() == even.size())
+	{
+		cout << 0 << endl;
+		return;
+	}
+	sort(odd.begin(), odd.end());
+	sort(even.begin(), even.end());
+	ll ans = 0;
+	flp(0, odd.size() - min(odd.size(), even.size()))
+	{
+		ans += odd[i];
+	}
+	flp(0, even.size() - min(odd.size(), even.size()))
+	{
+		ans += even[i];
+	}
+	ll extra = 0;
+	if(odd.size() > min(odd.size(), even.size()))
+		extra = max(extra, odd[odd.size() - min(odd.size(), even.size()) - 1]);
+	if(even.size() > min(odd.size(), even.size()))
+		extra = max(extra, even[even.size() - min(odd.size(), even.size()) - 1]);
+	ans -= extra;
+	cout << ans << endl;
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+}
