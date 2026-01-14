f414a073598eb4546859a729142c1a50d1ad7a34
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-01-14 21:31:59 +0530

    Create 2178A - Yes or Yes.cpp

---
+++ SOLVED/2178A - Yes or Yes.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k, n) for(ll i = (ll) k; i < (ll) n; i ++)
+#define flm(k, n) for(ll j = (ll) n - 1; j >= (ll) k; j --)
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout << "\n"
+#define SPACE << " " <<
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+
+vector<ll> adj[(ll)2e5 + 5];
+vector<bool> vis((ll)2e5 + 5, 0);
+vector<ll> vals((ll)2e5 + 5);
+
+ll dfs(ll node, ll depth)
+{
+    ll child = 0;
+    vis[node] = 1;
+    for(auto x : adj[node])
+    {
+        if(vis[x])
+            continue;
+        child += dfs(x, depth + 1);
+    }
+    vals[node] = depth - child;
+    return child + 1;
+}
+
+
+void solve() 
+{
+    string s;
+    cin >> s;
+    int y = count(s.begin(), s.end(), 'Y');
+    if (y > 1) 
+        NO;
+    else 
+        YES;
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+}
