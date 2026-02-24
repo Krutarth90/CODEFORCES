964f6d6f4e16acb9ee25440f305201fb8e5042b3
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-02-24 23:47:29 +0530

    Create 1455A - Strange Functions.cpp

---
+++ JavaScript/SOLVED/800/1455A - Strange Functions.cpp
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let output = [];
+
+    let n = next();
+    output.push(n.length)
+    console.log(output.join('\n'));
+}
+
+// ---------- MAIN ----------
+
+function main() {
+    
+
+    let TC = 1;
+    TC = Number(next());
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
