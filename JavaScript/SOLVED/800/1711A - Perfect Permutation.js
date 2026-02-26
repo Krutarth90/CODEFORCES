c0fed821962534a4db6b103e53e5de3d9a19f1cc
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-02-26 23:56:16 +0530

    Create 1711A - Perfect Permutation.js

---
+++ JavaScript/SOLVED/800/1711A - Perfect Permutation.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let output = [];
+
+    let temp = Number(next());
+    output.push(temp);
+    temp --;
+    for(let i = 1; i <= temp; i ++)
+        output.push(i);
+    
+    console.log(output.join(' '));
+}
+
+// ---------- MAIN ----------
+
+function main() {
+    
+
+    let TC = 1;
+    TC = Number(next());
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
