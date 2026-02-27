d7d0d9cd0120e56e967b8b863ed7bdc2ffb83199
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-02-27 23:27:12 +0530

    Create 1005A - Tanya and Stairways.cpp

---
+++ SOLVED/800/1005A - Tanya and Stairways.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define flp(k, n) for(ll i = (ll) k; i < (ll) n; i ++)
+#define flm(k, n) for(ll j = (ll) n - 1; j >= (ll) k; j --)
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define NL cout << "\n"
+#define SPACE << " " <<
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ll mod(ll x)
+{
+    return x % (998244353);
+}
+ 
+
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    vector<int> a;
+    int p = -1;
+    flp(0, n) {
+        int x;
+        cin >> x;
+        if (x == 1 && p != -1)
+            a.push_back(p);
+        p = x;    
+    }
+    a.push_back(p);
+    cout << a.size() << endl;
+    for (int i: a)
+        cout << i << " ";
+}
+
+signed main() 
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    int TC = 1;
+    // cin >> TC;
+    while(TC--)
+        solve();
+}
