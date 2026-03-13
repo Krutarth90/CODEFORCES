7091587a4377a1415a55dd75456316bd85e8eebf
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-03-13 23:49:20 +0530

    Create 2203A - Towers of Boxes.js

---
+++ JavaScript/SOLVED/2203A - Towers of Boxes.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let n = Number(next()),
+        m = Number(next()),
+        d = Number(next());
+    
+    console.log(Math.ceil(n / (Math.floor(d / m) + 1)));
+}
+
+// ---------- MAIN ----------
+
+function main() {
+    
+
+    let TC = 1;
+    TC = Number(next());
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
