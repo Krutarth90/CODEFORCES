573cf5dd94a20297d9967b065d82da4b881b998b
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-03-15 23:10:58 +0530

    Create 1680A - Minimums and Maximums.js

---
+++ JavaScript/SOLVED/800/1680A - Minimums and Maximums.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let l1 = +next(),
+        r1 = +next(),
+        l2 = +next(),
+        r2 = +next();
+    if(l2 > r1 || l1 > r2)
+        console.log(l2 + l1);
+    else
+        console.log(Math.max(l1, l2));
+}
+
+// ---------- MAIN ----------
+
+function main() {
+    
+
+    let TC = 1;
+    TC = Number(next());
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
