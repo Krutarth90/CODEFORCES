7088420e5c37018259bcd9e17bcbc9c9f6836d46
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-03-20 23:44:52 +0530

    Create 808A - Lucky Year.js

---
+++ JavaScript/SOLVED/900/808A - Lucky Year.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let x = next();
+    if(x.length == 1)
+    {
+        console.log(1);
+        return;
+    }
+    
+    console.log(10 ** (x.length - 1) - +x.slice(1))
+
+}
+
+// ---------- MAIN ----------
+
+function main() {
+    
+
+    let TC = 1;
+    TC = Number(next());
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
