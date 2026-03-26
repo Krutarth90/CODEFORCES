5f4ad7cfb97a1352ae0d080cadc2124d47d1a4ca
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-03-26 23:22:52 +0530

    Create 1998B - Minimize Equal Sum Subarrays.js

---
+++ JavaScript/SOLVED/1000/1998B - Minimize Equal Sum Subarrays.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let n = + next();
+    let arr = new Array(n);
+    for(let i = 0; i < n; i ++){
+        if(!i)
+            arr[n - 1] = + next();
+        else
+            arr[i - 1] = + next();  
+    }
+    console.log(arr.join(" "));
+}
+
+// ---------- MAIN ----------
+
+function main() {
+    
+
+    let TC = 1;
+    TC = Number(next());
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
