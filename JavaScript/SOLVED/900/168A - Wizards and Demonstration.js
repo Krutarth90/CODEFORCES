ac95e043ea3226bd22c24438a4b99ef3cc6a3b73
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-03-28 21:17:12 +0530

    Create 168A - Wizards and Demonstration.js

---
+++ JavaScript/SOLVED/900/168A - Wizards and Demonstration.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let n = +next(),
+        x = +next(),
+        y = +next();
+    let min = Math.ceil(y * n / 100);
+    min -= x;
+    if(min < 0)
+        min = 0;
+    console.log(min);
+}
+
+// ---------- MAIN ----------
+
+function main() {
+    
+
+    let TC = 1;
+    // TC = Number(next());
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
