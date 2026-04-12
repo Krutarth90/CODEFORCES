dec61bab66f4be1c165768361e16e48305423734
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-04-12 22:21:53 +0530

    Create 2211A - Antimedian Deletion.js

---
+++ JavaScript/SOLVED/2211A - Antimedian Deletion.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let n = +next();
+    let a = [];
+    for(let i = 0; i < n; i ++){
+        let x = +next();
+        a.push(2);
+    }
+    if(n === 1)
+        console.log(1);
+    else
+        console.log(...a);
+}
+
+// ---------- MAIN ----------
+
+function main() {
+    
+
+    let TC = 1;
+    TC = Number(next());
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
