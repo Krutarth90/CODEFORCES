10fca74b4b9044591240e2deae4ed1d300fc90e0
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-04-29 23:52:32 +0530

    Create 2200B - Deletion Sort.js

---
+++ JavaScript/SOLVED/800/2200B - Deletion Sort.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+function gcd(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return Math.abs(a);
+}
+
+function lcm(a, b) {
+  return Math.abs(a * b) / gcd(a, b);
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let n = + next(),
+        flag = false,
+        prev = 0;
+    for(let i = 0; i < n; i ++){
+        let x = + next();
+        if(x < prev){
+            flag = true;
+        }
+        prev = x;
+    }
+    console.log(flag ? 1 : n);
+}
+
+// ---------- MAIN ----------
+
+function main() {
+
+    let TC = 1;
+    TC = Number(next()); 
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
