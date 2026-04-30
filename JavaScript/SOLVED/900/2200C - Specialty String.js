77c887808c98fd9aa1a128172a8f85a3bf065589
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-04-30 22:33:32 +0530

    Create 2200C - Specialty String.js

---
+++ JavaScript/SOLVED/900/2200C - Specialty String.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+function gcd(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return Math.abs(a);
+}
+
+function lcm(a, b) {
+  return Math.abs(a * b) / gcd(a, b);
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let n = + next(),
+        s =  next(),
+        stack = [];
+    for(let c of s){
+        if(stack.length && stack[stack.length - 1] === c)   stack.pop();
+        else stack.push(c);
+    }
+    console.log(stack.length ? 'NO' : 'YES');
+}
+
+// ---------- MAIN ----------
+
+function main() {
+
+    let TC = 1;
+    TC = Number(next()); 
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
