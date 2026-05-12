1b4e7d6feaaff702a1dfcc1baa0aab26f5487458
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-05-12 22:59:54 +0530

    Create 2227A - Koshary.js

---
+++ JavaScript/SOLVED/2227A - Koshary.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+function gcd(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return Math.abs(a);
+}
+
+function lcm(a, b) {
+  return Math.abs(a * b) / gcd(a, b);
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let a = + next(),
+        b = + next();
+    console.log(a % 2 && b % 2 ? "NO" : "YES");
+}
+
+// ---------- MAIN ----------
+
+function main() {
+
+    let TC = 1;
+    TC = Number(next()); 
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
