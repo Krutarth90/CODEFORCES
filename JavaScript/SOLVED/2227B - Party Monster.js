bbe7d9597587010cfdb6e193d492c46f9bad9696
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-05-13 22:53:48 +0530

    Create 2227B - Party Monster.js

---
+++ JavaScript/SOLVED/2227B - Party Monster.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+function gcd(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return Math.abs(a);
+}
+
+function lcm(a, b) {
+  return Math.abs(a * b) / gcd(a, b);
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let a = + next(),
+        c = 0;
+    let s = next();
+    for(let x of s){
+        c += x === '(' ? 1 : -1;
+    }
+    console.log(c ? "NO" : "YES");
+}
+
+// ---------- MAIN ----------
+
+function main() {
+
+    let TC = 1;
+    TC = Number(next()); 
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
