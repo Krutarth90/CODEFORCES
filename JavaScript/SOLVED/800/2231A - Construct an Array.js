e725e56e1218d5841020fedd6a74c16ab1d048de
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-06-01 23:15:55 +0530

    Create 2231A - Construct an Array.js

---
+++ JavaScript/SOLVED/800/2231A - Construct an Array.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+function gcd(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return Math.abs(a);
+}
+
+function lcm(a, b) {
+  return Math.abs(a * b) / gcd(a, b);
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let n = + next(),
+        i = -1, 
+        ans = [];
+    while(n --){
+        ans.push(i += 2);
+    }
+    console.log(...ans);
+}
+
+// ---------- MAIN ----------
+
+function main() {
+
+    let TC = 1;
+    TC = Number(next());    
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
