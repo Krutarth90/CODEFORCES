b3b715b15f1f0cce7669aef14807459b81006953
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-06-03 22:58:12 +0530

    Create 2226B - Everything Everywhere.cpp

---
+++ SOLVED/900/2226B - Everything Everywhere.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+    int t;
+    cin >> t;
+ 
+    while(t--){
+        int n;
+        cin >> n;
+ 
+        vector<int> a(n);
+        for(int i = 0; i < n; i++) cin >> a[i];
+ 
+        int ans = 0;
+        for(int i = 0; i < n - 1; i++) if(a[i] % (a[i] - a[i + 1]) == 0) ans++;
+        
+        cout << ans << '\n';
+    }
+ 
+    return 0;
+}
