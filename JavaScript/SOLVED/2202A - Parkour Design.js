586268f950d35c8988f607acded8ff4e72846e1c
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-06-06 23:49:49 +0530

    Create 2202A - Parkour Design.js

---
+++ JavaScript/SOLVED/2202A - Parkour Design.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+function gcd(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return Math.abs(a);
+}
+
+function lcm(a, b) {
+  return Math.abs(a * b) / gcd(a, b);
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let x = + next(),
+        y = + next();
+    if ((x + y) % 3 != 0) {
+        console.log("NO");
+        return;
+    }
+        
+    let moves = Math.floor((x + y) / 3);
+    
+    if (-moves <= y && y <= moves) {
+        console.log("YES");
+    } else {
+        console.log("NO");
+    }
+
+
+}
+
+// ---------- MAIN ----------
+
+function main() {
+
+    let TC = 1;
+    TC = Number(next());    
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
