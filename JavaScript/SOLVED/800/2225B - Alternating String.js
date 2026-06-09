280c0cdd36e8be99a8fcbac07ba48597101dbd11
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-06-09 23:22:44 +0530

    Create 2225B - Alternating String.js

---
+++ JavaScript/SOLVED/800/2225B - Alternating String.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+function gcd(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return Math.abs(a);
+}
+
+function lcm(a, b) {
+  return Math.abs(a * b) / gcd(a, b);
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let s = next(),
+        c = 0;
+    for(let i = 0; i < s.length - 1; i ++)
+        if(s[i] === s[i + 1])
+            c ++;
+        
+    if(c < 3){
+        console.log('YES');
+        return;
+    }
+    console.log('NO');
+}
+
+// ---------- MAIN ----------
+
+function main() {
+
+    let TC = 1;
+    TC = Number(next());    
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
