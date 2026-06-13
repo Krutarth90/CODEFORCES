7742fd335594221f2cdf52985fcfe10ccf2a9ec0
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-06-13 23:39:18 +0530

    Create 2233A - AI Project Development.js

---
+++ JavaScript/SOLVED/800/2233A - AI Project Development.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+function next() {
+    return input[idx++];
+}
+
+// ---------- UTILS ----------
+
+// mod function
+function mod(x) {
+    return x % 998244353;
+}
+
+// YES / NO helpers
+function YES(output) {
+    output.push("YES");
+}
+
+function NO(output) {
+    output.push("NO");
+}
+
+function gcd(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return Math.abs(a);
+}
+
+function lcm(a, b) {
+  return Math.abs(a * b) / gcd(a, b);
+}
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let n = + next(),
+        x = + next(), 
+        y = + next(), 
+        t = + next();
+    let ans = Math.ceil(n / (x + y));
+    if(t * x < n){
+        ans = Math.min(ans, (t + Math.ceil((n - t * x) / (10 * y + x))));
+    }
+    console.log(ans);
+}
+
+// ---------- MAIN ----------
+
+function main() {
+
+    let TC = 1;
+    TC = Number(next());    
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
