6728a6a5e7dd08fecfc070b0f23ce990b77e8929
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-06-15 22:56:38 +0530

    Add solution for 'Games on the Train' problem

---
+++ JavaScript/SOLVED/2236A - Games on the Train.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+const next = () => input[idx++];
+
+// ---------- UTILS ----------
+
+
+function gcd(a, b) {
+    while (b !== 0) {
+        [a, b] = [b, a % b];
+    }
+    return Math.abs(a);
+}
+
+const mod = (x) => x % 998244353;
+
+const lcm = (a, b) => Math.abs(a * b) / gcd(a, b);
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let n = + next(), 
+        mi = 7, ma = 0;
+    for(let i = 0; i < n; i ++){
+        let x = + next();
+        mi = mi > x ? x : mi;
+        ma = ma < x ? x : ma;
+    }
+    console.log(ma - mi + 1);
+}
+
+// ---------- MAIN ----------
+
+function main() {
+
+    let TC = 1;
+    TC = Number(next());    
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
