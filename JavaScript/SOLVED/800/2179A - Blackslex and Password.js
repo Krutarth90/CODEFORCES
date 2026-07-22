e714966a52083f3ad555347ea4c8a792c5250a39
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-07-22 23:16:51 +0530

    Create 2179A - Blackslex and Password.js

---
+++ JavaScript/SOLVED/800/2179A - Blackslex and Password.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split(/\s+/);
+
+let idx = 0;
+const next = () => input[idx++];
+
+// ---------- UTILS ----------
+
+
+function gcd(a, b) {
+    while (b !== 0) {
+        [a, b] = [b, a % b];
+    }
+    return Math.abs(a);
+}
+
+const mod = (x) => x % 998244353;
+
+const lcm = (a, b) => Math.abs(a * b) / gcd(a, b);
+
+// ---------- SOLVE ----------
+
+function solve() {
+    let n = + next(),
+        m = + next();
+    console.log(n * m + 1);
+}
+
+// ---------- MAIN ----------
+
+function main() {
+
+    let TC = 1;
+    TC = Number(next());    
+
+    while (TC--) {
+        solve();
+    }
+  
+}
+
+main();
