6bc4f061de42afcb1150cc21afdfa07aaa623f0d
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-08-27 23:26:39 +0530

    Create 2257B - Gigantomachy.cpp

---
+++ SOLVED/800/2257B - Gigantomachy.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+using ll = long long;
+
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    ll a0, b0;
+    cin >> a0;
+    ll skip;
+    for (int i = 1; i < n; ++i) {
+        cin >> skip;
+    }
+    cin >> b0;
+    for (int i = 1; i < m; ++i) {
+        cin >> skip;
+    }
+    ll g1 = a0 + n;
+    ll g2 = b0 + m;
+    cout << ((g1 >= g2) ? 1 : 2);
+}
+
+int main() {
+    int tests;
+    cin >> tests;
+    while (tests--) {
+        solve();
+        cout << '\n';
+    }
+    return 0;
+}
