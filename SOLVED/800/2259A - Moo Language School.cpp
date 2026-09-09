cf08aba9faf0c38f298b204f3e67b7850e0a3ce5
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-09-09 21:01:50 +0530

    Create 2259A - Moo Language School.cpp

---
+++ SOLVED/800/2259A - Moo Language School.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        string s;
+        cin >> s;
+
+        int ans = 0;
+        for (int i = 0; i < n / k; i++) {
+            bool has_non_nhoj_farm = false;
+            for (int j = i * k; j < (i + 1) * k; j++) {
+                if (s[j] == '0') has_non_nhoj_farm = true;
+            }
+            ans += !has_non_nhoj_farm;
+        }
+
+        cout << ans << "\n";
+    }
+}
