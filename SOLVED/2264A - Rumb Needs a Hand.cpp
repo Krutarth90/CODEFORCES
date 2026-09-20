4f4f70aa0d7b2a78abea3862677f92efc8879e39
Krutarth Pipaliya <118007834+Krutarth90@users.noreply.github.com>
2026-09-20 22:01:07 +0530

    Create 2264A - Rumb Needs a Hand.cpp

---
+++ SOLVED/2264A - Rumb Needs a Hand.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> p(n);
+        for (int &x : p) cin >> x;
+
+        vector<int> bad;
+        for (int i = 0; i < n; ++i) {
+            if (p[i] != i + 1) {
+                bad.push_back(i);
+            }
+        }
+
+        bool ok = true;
+        int k = bad.size();
+        for (int i = 0; i < k; ++i) {
+            if (p[bad[i]] != bad[k - 1 - i] + 1) {
+                ok = false;
+                break;
+            }
+        }
+
+        cout << (ok ? "YES\n" : "NO\n");
+    }
+}
